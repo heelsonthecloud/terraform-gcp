1e67eff5b30f7c56136be108f3342187b5296b72
Özlem Altunel <ozlemaltunel@Ozlem-MacBook-Pro.local>
2020-02-04 08:09:57 +0300

    google cloud managed zone added

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "google" {
   credentials = file("terraform-plygrnd-7dcd713ddce7.json")
 
   project = "terraform-plygrnd"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  region  = "us-west1"
+  zone    = "us-west1-a"
 }
 
 resource "google_compute_network" "vpc_network" {
@@ -41,4 +41,26 @@ resource "google_compute_instance" "vm_instance" {
     access_config {
     }
   }
+}
+resource "google_dns_managed_zone" "heely-zone" {
+  name        = "heeled-zone"
+  dns_name    = "heelsonthecloud.com."
+  description = "Heely DNS zone"
+}
+
+resource "google_dns_record_set" "a" {
+  name         = "${google_dns_managed_zone.heely-zone.dns_name}"
+  managed_zone = google_dns_managed_zone.heely-zone.name
+  type         = "A"
+  ttl          = 300
+
+  rrdatas = [google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip]
+}
+
+resource "google_dns_record_set" "cname" {
+  name         = "www.${google_dns_managed_zone.heely-zone.dns_name}"
+  managed_zone = google_dns_managed_zone.heely-zone.name
+  type         = "CNAME"
+  ttl          = 300
+  rrdatas      = ["heelsonthecloud.com."]
 }
